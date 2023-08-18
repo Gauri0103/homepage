f12a6aad198d8afcb38e85b1187a2860e850012c
Kunal Sharma <S.kunal582002@gmail.com>
2023-08-19 00:39:16 +0530

    first

---
+++ src/modules/Trial.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Trial() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/640px-SNice.svg.png"
+          style={{ height: "100px", width: "100px" }}
+          alt="Image 1"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/640px-SNice.svg.png"
+          style={{ height: "100px", width: "100px" }}
+          alt="Image 2"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/640px-SNice.svg.png"
+          style={{ height: "100px", width: "100px" }}
+          alt="Image 3"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/640px-SNice.svg.png"
+          style={{ height: "100px", width: "100px" }}
+          alt="Image 4"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/640px-SNice.svg.png"
+          style={{ height: "100px", width: "100px" }}
+          alt="Image 5"
+        />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/640px-SNice.svg.png"
+          style={{ height: "100px", width: "100px" }}
+          alt="Image 6"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Trial;
